45038f31d41e4490c22052644eae328b597f6a27
Bernhard Breuß <bernhard.breuss@students.fhv.at>
2013-03-06 15:57:56 +0100

    first implementation

---
+++ main.c
@@ -0,0 +1,21 @@
+/*
+ * main.c
+ */
+typedef volatile unsigned int* address;
+
+#define GPIO5_DIR  			(address) 0x49056094
+#define LED0_PIN			(1 << 21)
+#define LED1_PIN			(1 << 22)
+#define GPIO5_OUT			(address) 0x4905603C
+
+void main(void) {
+	*(GPIO5_DIR) |= (LED0_PIN | LED1_PIN);
+	*(GPIO5_OUT) &= ~LED1_PIN;
+	int i;
+
+	while(1) {
+		for(i = 0; i < 200000; i++);
+
+		*(GPIO5_OUT) ^= (LED0_PIN | LED1_PIN);
+	}
+}
